4575645e2de565a26e2c53b05ca4b41e55e02202
Diego <20460349@colima.tecnm.mx>
2024-04-16 02:15:42 -0600

    CREATED login screen

--- App.tsx
+++ App.tsx
@@ -1,106 +1,24 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
+    SafeAreaView,
+    StyleSheet,
+    Text,
 } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import SignInScreen from './src/screens/SignInScreen';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        {/* Header */}
-        <View style={styles.header}>
-          <TouchableOpacity onPress={() => console.log('Ir a la información del usuario')}>
-            <View style={styles.iconButton}>
-              {/* Icono de usuario */}
-              <Text style={styles.iconText}>Usuario</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('Ir a los ajustes')}>
-            <View style={styles.iconButton}>
-              {/* Icono de ajustes */}
-              <Text style={styles.iconText}>Ajustes</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        {/* Contenido principal */}
-        <View style={styles.content}>
-          {/* Aquí irá el resumen de productos e materiales */}
-          <Text style={styles.summary}>
-            Resumen de productos e materiales ingresados
-          </Text>
-          {/* Puedes agregar más componentes aquí para mostrar el resumen */}
-        </View>
-      </ScrollView>
-
-      {/* Botón de opciones */}
-      <TouchableOpacity onPress={() => console.log('Ir a las opciones del gestor')} style={styles.optionButton}>
-        <Text style={styles.optionText}>Opciones</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+const Authentication = () => {
+    return (
+        <SafeAreaView style = {styles.root}>
+            <SignInScreen />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-  },
-  iconButton: {
-    backgroundColor: 'lightblue',
-    borderRadius: 25,
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  iconText: {
-    fontSize: 13,
-    fontWeight: 'bold',
-  },
-  content: {
-    flex: 1, // Usa todo el espacio disponible
-    justifyContent: 'center', // Centra el contenido verticalmente
-    alignItems: 'center', // Centra el contenido horizontalmente
-    padding: 20,
-  },
-  summary: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  optionButton: {
-    backgroundColor: 'lightgreen',
-    alignItems: 'center',
-    padding: 15,
-    margin: 10, // Agregamos un margen para separar el botón del contenido
-    alignSelf: 'center', // Lo alineamos al centro horizontalmente
-  },
-  optionText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  root: {
+    flex: 1,
+    backgroundColor: '#F9FBFC',
   },
 });
 
-export default App;
+export default Authentication;
---
+++ src/screens/SignInScreen/SignInScreen.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from "react-native";
+import Logo from '../../../assets/images/Logo_1.png'
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton";
+
+const SignInScreen = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {height} = useWindowDimensions();
+
+    const onSignInPressed = () => {
+        console.warn("Sign In");
+    }
+
+    const onForgotPasswordPressed = () => {
+        console.warn("Forgot Password");
+    }
+
+    const onSignInGoogle = () => {
+        console.warn("Sign In with Google");
+    }
+
+    const onSignUp = () => {
+        console.warn("Create Account");
+    }
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.root}>
+            <Image 
+            source={Logo} 
+            style={[styles.logo, {height: height * 0.3}]} 
+            resizeMode="contain" 
+            />
+
+            <CustomInput 
+            placeholder="Username" 
+            value={username} 
+            setValue={setUsername} 
+            secureTextEntry={false}
+            />
+
+            <CustomInput 
+            placeholder="Password" 
+            value={password} 
+            setValue={setPassword} 
+            secureTextEntry={true}
+            />
+
+            <CustomButton 
+            text="Sign In" 
+            onPress={onSignInPressed} 
+            />
+
+            <CustomButton 
+            text="Forgot password?" 
+            onPress={onForgotPasswordPressed} 
+            type="TERTIARY" />
+
+            <CustomButton 
+            text="Sign In with Google" 
+            onPress={onSignInGoogle} 
+            bgColor="#FAE9EA"
+            fgColor="#DD4D44"
+            />
+            <CustomButton 
+            text="Don't have an account? Create one" 
+            onPress={onSignUp} 
+            type="TERTIARY" 
+            />
+
+        </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 40,
+    },
+    logo: {
+        width: '70%',
+        maxWidth: 500,
+        height: 100,
+        maxHeight: 200,
+        marginBottom: 15,
+    },
+});
+
+export default SignInScreen;
